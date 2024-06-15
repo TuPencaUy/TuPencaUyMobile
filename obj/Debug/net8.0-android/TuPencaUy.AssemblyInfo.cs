c7788cb59513a4c44a668e5bbe6991bfd074a9fa
Nitram13E <martinquot@gmail.com>
2024-06-15 04:44:29 -0300

    gitignore agregado.

--- obj/Debug/net8.0-android/TuPencaUy.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/TuPencaUy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TuPencaUy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+93cf8e756dbf3c7150c35521111a6c682a89b5a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d01ed55e34bc2912cf9a34fc39f4e828c2be92e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TuPencaUy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TuPencaUy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
